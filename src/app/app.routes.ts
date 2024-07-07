aab7698cd9de8e6a08b24c8bcf6e5d3ebbf51db4
Mohamed Walid <mohamedwalidtharwat2020@gmail.com>
2024-07-07 23:12:59 +0300

    structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', loadComponent: () => import('./pages/timelines/timeline/timeline.component').then(c => c.TimelineComponent)},
+  { path: 'login', loadComponent: () => import('./pages/auth/login/login.component').then(c => c.LoginComponent)},
+  { path: 'register', loadComponent: () => import('./pages/auth/register/register.component').then(c => c.RegisterComponent)},
+  { path: '**', loadComponent: () => import('./pages/shared/notfound/notfound.component').then(c => c.NotfoundComponent)},
+];
---
+++ src/app/pages/timelines/timeline-card/timeline-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timeline-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './timeline-card.component.html',
+  styleUrl: './timeline-card.component.scss'
+})
+export class TimelineCardComponent {
+
+}
